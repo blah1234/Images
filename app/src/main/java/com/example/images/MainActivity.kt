a13bc564e242265ba40f9f3c8ecf5713a50195c9
Shawn Hwang <shawn_hwang@apple.com>
2024-06-10 14:42:27 -0700

    - using better DataBindingUtil method to set activity content

--- app/src/main/java/com/example/images/MainActivity.kt
+++ app/src/main/java/com/example/images/MainActivity.kt
@@ -26,7 +26,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = DataBindingUtil.inflate<ActivityMainBinding>(layoutInflater, R.layout.activity_main, null, false)
-        setContentView(binding.root)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
