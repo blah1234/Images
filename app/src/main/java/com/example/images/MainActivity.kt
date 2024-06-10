733489ea486585df5e423fa2060ecad77d2f4cd6
Shawn Hwang <shawn_hwang@apple.com>
2024-06-10 14:41:41 -0700

    - configure if allowing network access on main thread

--- app/src/main/java/com/example/images/MainActivity.kt
+++ app/src/main/java/com/example/images/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.images
 
 import android.os.Bundle
+import android.os.StrictMode
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -8,7 +9,19 @@ import com.example.images.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val ALLOW_NETWORK_ON_MAIN_TREAD = true
+    }
+
     private val viewModel by viewModels<DataViewModel>()
+
+
+    init {
+        if (ALLOW_NETWORK_ON_MAIN_TREAD) {
+            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
